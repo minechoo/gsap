05c8e79c0deca8f36217ef3b4bbae041b762d91c
tubi55 <tubi55@nate.com>
2023-09-15 12:02:04 +0900

    기존 사용패턴2

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const wrap = document.querySelector('.wrap');
 
+/*
+basic use
 gsap.set('.box', { x: -200, opacity: 0 });
 
 wrap.addEventListener('click', () => {
 	gsap.fromTo('.box', { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
+});
+*/
+
+//bg, transform-origin, duration, relativePosition
+wrap.addEventListener('click', () => {
+	//gsap.to('.box', { rotation: 360, backgroundColor: '#ffff00', duration: 2, transformOrigin: '0%  0%' });
+	gsap.to('.box', { x: 500 });
+	gsap.to('.box2', { x: 500, delay: 0 });
+	gsap.to('.box3', { x: 500, repeat: 5, yoyo: true, ease: 'bounce-out' });
 });
