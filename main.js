5dbf8a413a6ddc0a5e6189583145ec1d1eb6b36e
tubi55 <tubi55@nate.com>
2023-09-15 12:16:21 +0900

    반복되는 모션 정보 배열로 관리

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 const wrap = document.querySelector('.wrap');
+const boxs = wrap.querySelectorAll('div');
 
-/*
-basic use
-gsap.set('.box', { x: -200, opacity: 0 });
-
-wrap.addEventListener('click', () => {
-	gsap.fromTo('.box', { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
-});
-*/
+const motionInfo = [
+	{ x: 700, opacity: 1, duration: 2 },
+	{ x: 700, scale: 1.5, opacity: 1, duration: 0.5 },
+	{ x: 700, opacity: 1, rotation: 150, duration: 1.5 },
+	{ x: 700, opacity: 1, scale: 0.5, duration: 0.2 },
+	{ x: 700, opacity: 1, y: 300, duration: 1.8 },
+];
 
 //bg, transform-origin, duration, relativePosition
 const tl = gsap.timeline();
-wrap.addEventListener('click', () => {
-	tl.to('.box', { x: 700, opacity: 1, duration: 2 });
-	tl.to('.box2', { x: 700, scale: 1.5, opacity: 1, duration: 0.5 });
-	tl.to('.box3', { x: 700, opacity: 1, rotation: 150, duration: 1.5 });
-	tl.to('.box4', { x: 700, opacity: 1, scale: 0.5, duration: 0.2 });
-	tl.to('.box5', { x: 700, opacity: 1, y: 300, duration: 1.8 });
-});
+wrap.addEventListener('click', () => motionInfo.forEach((data, idx) => tl.to(boxs[idx], data)));
