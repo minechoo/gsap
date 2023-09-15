a2d5005831bf6a86d8bbf5b34e37983a66957462
tubi55 <tubi55@nate.com>
2023-09-15 13:14:46 +0900

    timeline과 조합해서 스크롤 모션 동기화 처리

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-gsap.from('.box', {
-	duration: 2,
-	x: 500,
-	backgroundColor: '#00ffff',
+let split = new SplitText('h1');
+console.log(split);
 
-	//scrollTrigger 위치값을 따로 지정하지 않으면
-	//기본적으로 trigger로 지정한 요소가 viewport에 들어오기만 하면 바로 실행됨
+let tl = gsap.timeline({
 	scrollTrigger: {
-		trigger: '.box',
+		trigger: '#sec2',
+		start: 'top 50%',
+		end: 'top 0%',
 		markers: true,
-		start: 'top 50%', //해당 요소의 모션이 시작될 viewport상의 세로위치 지정
-		end: 'top 10%', //해당 요소의 모션 적용이 해제될 viewport상의 세로위치 지정
-		toggleActions: 'restart pause reverse reset', //영역진입, 영역떠남, 재영역진입, 재영역떠남
-		//play: 모션시작
-		//pause: 모션일시정지
-		//resume: 모션이어서 실행
-		//restart: 모션 다시 처음부터 시작
-		//complete: 무조건 마지막 위치로 모션강제 종료
-		//reverse: 다시 역으로 모션 재생
-		//none: 모션기능 해제
+		toggleActions: 'restart pause reverse reset',
 	},
 });
+
+tl.from(split.chars, { scale: 2, opacity: 0, stagger: 0.1 });
+tl.from('button', { x: 500, opacity: 0 });
