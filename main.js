b00269568c6e67a123bf53a0102710a51ef28fc7
tubi55 <tubi55@nate.com>
2023-09-15 12:12:30 +0900

    timeline을 통한 모션 동기화

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ wrap.addEventListener('click', () => {
 */
 
 //bg, transform-origin, duration, relativePosition
+const tl = gsap.timeline();
 wrap.addEventListener('click', () => {
-	gsap.to('div', {
-		duration: 1,
-		x: 700,
-		opacity: 1,
-		stagger: 0.2,
-	});
+	tl.to('.box', { x: 700, opacity: 1, duration: 2 });
+	tl.to('.box2', { x: 700, scale: 1.5, opacity: 1, duration: 0.5 });
+	tl.to('.box3', { x: 700, opacity: 1, rotation: 150, duration: 1.5 });
+	tl.to('.box4', { x: 700, opacity: 1, scale: 0.5, duration: 0.2 });
+	tl.to('.box5', { x: 700, opacity: 1, y: 300, duration: 1.8 });
 });
