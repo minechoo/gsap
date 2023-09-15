999731212d4cfbe67d901ce020430e26c1bb4e51
tubi55 <tubi55@nate.com>
2023-09-15 13:18:41 +0900

    기본 scrub 지정

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let split = new SplitText('h1');
-console.log(split);
-
 let tl = gsap.timeline({
 	scrollTrigger: {
 		trigger: '#sec2',
 		start: 'top 50%',
 		end: 'top 0%',
 		markers: true,
-		toggleActions: 'restart pause reverse reset',
+		//toggleActions: 'restart pause reverse reset',
+		scrub: 1,
 	},
 });
 
-tl.from(split.chars, { scale: 2, opacity: 0, stagger: 0.1 });
-tl.from('button', { x: 500, opacity: 0 });
+tl.to('.box', {
+	scale: 3,
+	opacity: 0,
+	rotation: 360,
+});
